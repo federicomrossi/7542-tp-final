ec63aa043f91f8106cfcd1a11dfba3d2ecdfad1b
Belen Beltran <belubeltran@gmail.com>
2013-06-09 03:56:28 -0300

    Implementacion Hash

--- Codigo/common_hash.cpp
+++ Codigo/common_hash.cpp
@@ -8,7 +8,10 @@
 
 #include "common_hash.h"
 
-
+namespace {
+	#define TAM_HASH 20
+	#define TAM_HEXA 41
+}
 
 
 /* ****************************************************************************
@@ -17,5 +20,32 @@
 
 // Aplica la función de hash al string entrante
 std::string Hash::funcionDeHash(std::string cadena) {
-	return cadena;
+	unsigned char hash[TAM_HASH];
+	char hash_hexa[TAM_HEXA];
+
+	// Se inicializan las variables
+	memset(hash, '\0', TAM_HASH);
+	memset(hash_hexa, '\0', TAM_HEXA);
+
+	// Se calcula el hash con el algoritmo sha1
+	sha1::calc((void*)cadena.c_str(), cadena.length(), hash);
+
+	// Se transforma el hash a una cadena en hexadecimal
+	return(Convertir::uitoh(hash, TAM_HASH));
+}
+
+// Aplica la función de hash al char* entrante
+std::string Hash::funcionDeHash(const char* cadena, int longitud) {
+	unsigned char hash[TAM_HASH];
+	char hash_hexa[TAM_HEXA];
+
+	// Se inicializan las variables
+	memset(hash, '\0', TAM_HASH);
+	memset(hash_hexa, '\0', TAM_HEXA);
+
+	// Se calcula el hash con el algoritmo sha1
+	sha1::calc((void*)cadena, (unsigned int)longitud, hash);
+
+	// Se transforma el hash a una cadena en hexadecimal
+	return(Convertir::uitoh(hash, TAM_HASH));
 }
