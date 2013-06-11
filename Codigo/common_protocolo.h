1145eb860fe2a38310db8ec304ad57f51acaffa9
Belen Beltran <belubeltran@gmail.com>
2013-06-11 00:30:04 -0300

    Modificacion en protocolo

--- Codigo/common_protocolo.h
+++ Codigo/common_protocolo.h
@@ -44,7 +44,7 @@ const std::string COMMON_MSG_IS_FILE = "f";
 const std::string COMMON_SEND_FILE = "SEND-FILE";
 const std::string COMMON_MODIFY_FILE = "MODIFY-FILE";
 const std::string COMMON_DELETE_FILE = "DELETE-FILE";
-const char COMMON_FILE_PARAMETER_DELIMITER = ',';
+const char COMMON_DELIMITER = ',';
 
 // Constante para caracter de fin de instrucción
 const char FIN_MENSAJE = '\n';
